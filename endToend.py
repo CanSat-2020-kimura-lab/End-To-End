ec905b228ff914ce49c95af1feb61eb54df455ac
Okou583 <63486800+Okou583@users.noreply.github.com>
2020-07-27 13:32:39 +0900

    Update endToend.py

--- endToend.py
+++ endToend.py
@@ -13,6 +13,7 @@ import time
 import serial
 import pigpio
 import traceback
+from threading import Thread
 
 import BME280
 import GPS
@@ -54,6 +55,10 @@ releaselog = 'home/pi/log/releaseLog.txt'
 landingLog = 'home/pi/log/landingLog.txt'
 meltingLog = 'home/pi/log/meltingLog.txt'
 errorLog = 'home/pi/log/errorLog.txt'
+	
+#--- difine goal latitude and longitude ---#
+lon2 = 139.5430
+lat2 = 35.553
 
 pi = pigpio.pi()
 
@@ -73,6 +78,11 @@ def close():
 	pi.write(22,0)
 	pi.write(24,0)
 	GPS.close()
+
+def timer(t):
+	global cond
+	time.sleep(t)
+	cond = False
 
 if __name__ == '__main__'
 	try:
@@ -128,3 +138,149 @@ if __name__ == '__main__'
 				Pressjudge,Presscount = Land.pressdetect()
 				GPSjudge,GAcount = Land.gpsdetect()
 
+	#--- Run Phase ---#
+	direction = Calibration.calculate_direction(lon2,lat2)
+	goal_distance = direction["distance"]
+	#------------- GPS navigate -------------#
+	while goal_distance >= 5:
+		#------------- Calibration -------------#
+		#--- calculate offset ---#
+		magdata = Calibration.magdata_matrix()
+		magdata_offset = Calibration.calculate_offset(magdata)
+		magx_off = magdata_offset[3]
+		magy_off = magdata_offset[4]
+		magz_off = magdata_offset[5]
+		time.sleep(1)
+		#--- calculate θ ---#
+		data = Calibration.get_data()
+		magx = data[0]
+		magy = data[1]
+		magz = data[2]
+		accx = data[3]
+		accy = data[4]
+		accz = data[5]
+		#--- 0 <= θ <= 360 ---#
+		θ = Calibration.calculate_angle_2D(magx,magy,magx_off,magy_off)
+		#θ = calculate_angle_3D(accx,accy,accz,magx,magy,magz,magx_off,magy_off,magz_off)
+		#------------- rotate contorol -------------#
+		Calibration.rotate_control(θ,lon2,lat2)
+		location = Stuck.stuck_detection1()
+		longitude_past = location[0]
+		latitude_past = location[1]
+
+		#------------- run straight -------------#
+		for i in range(2):
+			try:
+				for i in range(2):
+					run = pwm_control.Run()
+					run.straight_h()
+					time.sleep(1)
+					#--- Send GPS data ---#
+					try:
+						while True:
+							value = GPS.readGPS()
+							latitude_new = value[1]
+							longitude_new = value[2]
+							print(value)
+							print('longitude = '+str(longitude_new))
+							print('latitude = '+str(latitude_new))
+							time.sleep(1)
+							if latitude_new != -1.0 and longitude_new != 0.0 :
+								break
+					except KeyboardInterrupt:
+						GPS.closeGPS()
+						print("\r\nKeyboard Intruppted, Serial Closed")
+
+					except:
+						GPS.closeGPS()
+						print (traceback.format_exc())
+					
+					IM920.Send(GPS_data)
+					#--- calculate goal direction ---#
+					direction = Calibration.calculate_direction(lon2,lat2)
+					goal_distance = direction["distance"]
+					if goal_distance <= 5:
+						break
+					#--- 0 <= azimuth <= 360 ---#
+					azimuth = direction["azimuth1"]
+					#--- calculate θ ---#
+					data = Calibration.get_data()
+					magx = data[0]
+					magy = data[1]
+					magz = data[2]
+					accx = data[3]
+					accy = data[4]
+					accz = data[5]
+					#--- 0 <= θ <= 360 ---#
+					θ = Calibration.calculate_angle_2D(magx,magy,magx_off,magy_off)
+					#θ = calculate_angle_3D(accx,accy,accz,magx,magy,magz,magx_off,magy_off,magz_off)
+
+					#--- if rover go wide left, turn right ---#
+					#--- 15 <= azimuth <= 360 ---#
+					if azimuth - 15 > θ and azimuth - 15 >= 0:
+						run = pwm_control.Run()
+						run.turn_right()
+						time.sleep(0.5)
+					#--- 0 <= azimuth < 15 ---#
+					elif azimuth - 15 < 0:
+						azimuth += 360
+						if azimuth - 15 > θ:
+							run = pwm_control.Run()
+							run.turn_right()
+							time.sleep(0.5)							
+
+					#--- if rover go wide right, turn left ---#
+					#--- 0 <= azimuth <= 345 ---#
+					if θ > azimuth + 15 and  azimuth + 15 > 360:
+						run = pwm_control.Run()
+						run.turn_left()
+						time.sleep(0.5)
+					#--- 345 < azimuth <= 360 ---#
+					elif azimuth + 15 > 360:
+						azimuth -= 360
+						if θ > azimuth + 15:
+							run = pwm_control.Run()
+							run.turn_left()
+							time.sleep(0.5)
+					#--- stuck detection ---#
+					moved_distance = Stuck.stuck_detection2(longitude_past,latitude_past)
+					if moved_distance >= 5:
+						IM920.Send("rover moved!")
+					else:
+						#--- stuck escape ---#
+						Stuck.stuck_escape()
+					
+					#--- Send GPS data ---#
+					try:
+						while True:
+							value = GPS.readGPS()
+							latitude_new = value[1]
+							longitude_new = value[2]
+							print(value)
+							print('longitude = '+str(longitude_new))
+							print('latitude = '+str(latitude_new))
+							time.sleep(1)
+							if latitude_new != -1.0 and longitude_new != 0.0 :
+								break
+					except KeyboardInterrupt:
+						GPS.closeGPS()
+						print("\r\nKeyboard Intruppted, Serial Closed")
+
+					except:
+						GPS.closeGPS()
+						print (traceback.format_exc())
+					
+					IM920.Send(GPS_data)
+					#--- calculate  goal direction ---#
+					direction = Calibration.calculate_direction(lon2,lat2)
+					goal_distance = direction["distance"]
+					if goal_distance <= 5:
+						break
+				      
+			except KeyboardInterrupt:
+				run = pwm_control.Run()
+				run.stop()
+			
+			finally:
+				run = pwm_control.Run()
+				run.stop()
